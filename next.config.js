89eae3a5529f8a389832377516844dab919fdb60
EdenChang <k307849@gmail.com>
2025-12-24 17:26:18 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 export const nextConfig = {
+  output: 'standalone',
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
